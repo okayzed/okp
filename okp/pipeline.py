1390fe68f90834f77fc98f6acbc0eaeb1f856043
okay <okay@chalk>
2020-08-08 11:11:29 -0700

    [dev] refactor pipeline to reduce redundant lines

--- okp/pipeline.py
+++ okp/pipeline.py
@@ -6,77 +6,55 @@ from . import util
 import os
 
 ASSERT_SOURCE_MAP = True
+
+
 def pipeline(lines, base_dir=None, add_source_map=True, fname=None):
-    lines = comments.skip_comments(lines) # can change line numbers
-    tlines = [(i+1, line) for i,line in enumerate(lines)]
-    tlines = structure.join_backslash_lines(tlines) # can change tline numbers
-    tlines = structure.join_open_bracketed_lines(tlines) # can change tline numbers
-    tlines = structure.join_percent_bracketed_lines(tlines) # can change tline  numbers
-    lines = [line for line_no,line in tlines]
-    line_nos = [line_no for line_no,_ in tlines]
+    lines = comments.skip_comments(lines)
+    # all functions modifying tlines can change the line numbers
+    # of the source code by removing or adding new lines
+    tlines = [(i+1, line) for i, line in enumerate(lines)]
+    tlines = structure.join_backslash_lines(tlines)
+    tlines = structure.join_open_bracketed_lines(tlines)
+    tlines = structure.join_percent_bracketed_lines(tlines)
+    lines = [line for line_no, line in tlines]
+    line_nos = [line_no for line_no, _ in tlines]
     num_lines = len(lines)
 
-    lines = structure.add_preceding_ignore_chars(lines) # replaces ```
-    if add_source_map and ASSERT_SOURCE_MAP:
-      assert(len(lines) == num_lines)
+    def runner(lines):
+        def run(step, *args):
+            new_lines = step(lines, *args)
+            if add_source_map and ASSERT_SOURCE_MAP:
+                assert(len(new_lines) == num_lines)
 
-    lines = keywords.replace_raw(lines, base_dir or os.getcwd())
-    if add_source_map and ASSERT_SOURCE_MAP:
-      assert(len(lines) == num_lines)
-    lines = keywords.replace_tabs(lines)
-    if add_source_map and ASSERT_SOURCE_MAP:
-      assert(len(lines) == num_lines)
-    lines = keywords.replace_pass(lines)
-    if add_source_map and ASSERT_SOURCE_MAP:
-      assert(len(lines) == num_lines)
-    lines = keywords.replace_for_shorthand(lines)
-    if add_source_map and ASSERT_SOURCE_MAP:
-      assert(len(lines) == num_lines)
-    lines = keywords.replace_blocks(lines)
-    if add_source_map and ASSERT_SOURCE_MAP:
-      assert(len(lines) == num_lines)
-    lines = keywords.replace_defs(lines)
-    if add_source_map and ASSERT_SOURCE_MAP:
-      assert(len(lines) == num_lines)
-    lines = keywords.replace_self(lines)
-    if add_source_map and ASSERT_SOURCE_MAP:
-      assert(len(lines) == num_lines)
+            del lines[:]
+            lines.extend(new_lines)
+            return lines
+
+        return run
+
+    run = runner(lines)
+    run(structure.add_preceding_ignore_chars)
+    run(keywords.replace_raw, base_dir or os.getcwd())
+    run(keywords.replace_tabs)
+    run(keywords.replace_pass)
+    run(keywords.replace_for_shorthand)
+    run(keywords.replace_blocks)
+    run(keywords.replace_defs)
+    run(keywords.replace_self)
 
     # replaces !, ?, ??, print, read, etc
-    lines = io.replace_io_keywords(lines)
-    if add_source_map and ASSERT_SOURCE_MAP:
-      assert(len(lines) == num_lines)
-
+    run(io.replace_io_keywords)
     # scopings is a per line scope of seen variables
-    lines = variables.replace_walrus_operator(lines)
-    if add_source_map and ASSERT_SOURCE_MAP:
-      assert(len(lines) == num_lines)
-    lines = variables.add_auto_declarations(lines) # can change line numbers
-    if add_source_map and ASSERT_SOURCE_MAP:
-      assert(len(lines) == num_lines)
-
+    run(variables.replace_walrus_operator)
+    run(variables.add_auto_declarations)
     # known keyword replacement has to happen after auto declarations
-    lines = keywords.replace_knowns(lines)
-    if add_source_map and ASSERT_SOURCE_MAP:
-      assert(len(lines) == num_lines)
-
-    lines = structure.add_parentheses(lines)
-    if add_source_map and ASSERT_SOURCE_MAP:
-      assert(len(lines) == num_lines)
-
-    lines = structure.add_trailing_semicolons(lines)
-    if add_source_map and ASSERT_SOURCE_MAP:
-      assert(len(lines) == num_lines)
-
+    run(keywords.replace_knowns)
+    run(structure.add_parentheses)
+    run(structure.add_trailing_semicolons)
     # add curly braces (from indentation) has to be last
-    lines = structure.add_curly_braces(lines)  # can change line numbers
-    if add_source_map and ASSERT_SOURCE_MAP:
-      assert(len(lines) == num_lines)
-
+    run(structure.add_curly_braces)  # can change line numbers
     # TODO: decide on whether to remove from generated code or not
-    lines = structure.remove_preceding_ignore_chars(lines)
-    if add_source_map and ASSERT_SOURCE_MAP:
-      assert(len(lines) == num_lines)
+    run(structure.remove_preceding_ignore_chars)
 
     requires = analysis.guess_required_files(lines)
     lines = requires + lines
